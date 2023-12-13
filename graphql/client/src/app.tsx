8c3d105987bb591126c2d35e375b88c64a9ec621
Bailig <barrybailig@hotmail.com>
2023-12-13 20:50:09 +0000

    added chat ui

--- graphql/client/src/app.tsx
+++ graphql/client/src/app.tsx
@@ -1,14 +1,40 @@
 export const App = () => {
   return (
-    <>
-      <div className="chat chat-start">
-        <div className="chat-bubble">
-          It's over Anakin, <br />I have the high ground.
+    <div className="container mx-auto min-h-screen grid place-items-center">
+      <div className="card w-96 bg-base-300 shadow-xl p-5 gap-4">
+        <div className="chat chat-start">
+          <div className="chat-bubble">
+            It's over Anakin, <br />I have the high ground.
+          </div>
+        </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble">You underestimate my power!</div>
+        </div>
+
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Type here..."
+            className="input w-full"
+          />
+          <button className="btn btn-square btn-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-      <div className="chat chat-end">
-        <div className="chat-bubble">You underestimate my power!</div>
-      </div>
-    </>
+    </div>
   );
 };
